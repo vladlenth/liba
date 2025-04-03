c8dbd418b425ac5d98efb6cf192be5d3c5bb1656
Vladlen Basenko <vladbas445@gmail.com>
2025-04-03 15:14:37 +0300

    commit

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //liba
-
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -8,34 +7,44 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
+
     this.isRead = function() {
-        if (this.read === true) {
-        return "already read";
+        if (this.read) {
+            return "already read";
         } else {
-        return "not read yet";
+            return "not read yet";
         }
     };
+
     this.info = function() {
         return `The ${this.title} by ${this.author}, ${this.pages} pages, ${this.isRead()}.`;
-        };
     };
+}
 
+//add book to liba
 function addBookToLibrary(title, author, pages, read) {
     const newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
 }
 
-myLibrary.forEach(Book => {
-    console.log(Book.info());
-});
+//display books
+function displayBooks() {
+    const outputDiv = document.querySelector(".book-list");
+    outputDiv.innerHTML = "";
 
+    myLibrary.forEach((book, index) => {
+        const bookInfo = document.createElement("div");
+        bookInfo.classList.add("column");
+        bookInfo.id = `output-${index}`;
+        bookInfo.textContent = book.info();
+        outputDiv.appendChild(bookInfo);
+    });
+}
 
 //modal
-
-
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("addBookBtn");
-var span = document.getElementById("closeModal");
+const modal = document.getElementById("myModal");
+const btn = document.getElementById("addBookBtn");
+const span = document.getElementById("closeModal");
 
 btn.onclick = function() {
     modal.style.display = "block";
@@ -51,13 +60,18 @@ window.onclick = function(event) {
     }
 }
 
+//event submit
 document.getElementById("bookForm").onsubmit = function(event) {
     event.preventDefault();
-    alert("Book added:\nTitle: " + document.getElementById("title").value + 
-          "\nAuthor: " + document.getElementById("author").value + 
-          "\nPages: " + document.getElementById("pages").value);
-    
+
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    const read = document.getElementById("read").checked;
+
+    addBookToLibrary(title, author, pages, read);
+    displayBooks();
+
     modal.style.display = "none";
-    
     this.reset();
 }
